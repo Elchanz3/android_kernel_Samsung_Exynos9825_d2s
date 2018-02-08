c4393d929534fefcce64507bd72d452c068f5421
Amit Kucheria <amit.kucheria@linaro.org>
2018-02-08 17:03:21 +0530

    drivers: thermal: Initialize thermal core earlier
    Modern devices have two contradictory boot requirements - boot as
    quickly as possible (which often means runs as fast as possible) while
    staying under the thermal envelope.
    
    Now that we've moved cpufreq initialisation and supported platform
    drivers to core_initcall, let us move the thermal framework
    initialization earlier too.
    
    However, netlink initialization happens only as part of fs_initcall so
    it has a userspace available. Initialize netlink events separately,
    later in the boot process.
    
    Originally-by: Lina Iyer <ilina@codeaurora.org>
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1535,13 +1535,8 @@ static int __init genetlink_init(void)
 	return genl_register_family(&thermal_event_genl_family);
 }
 
-static void genetlink_exit(void)
-{
-	genl_unregister_family(&thermal_event_genl_family);
-}
 #else /* !CONFIG_NET */
 static inline int genetlink_init(void) { return 0; }
-static inline void genetlink_exit(void) {}
 #endif /* !CONFIG_NET */
 
 static int thermal_pm_notify(struct notifier_block *nb,
@@ -1633,7 +1628,6 @@ static void __exit thermal_exit(void)
 {
 	unregister_pm_notifier(&thermal_pm_nb);
 	of_thermal_destroy_zones();
-	genetlink_exit();
 	class_unregister(&thermal_class);
 	thermal_unregister_governors();
 	ida_destroy(&thermal_tz_ida);
@@ -1642,5 +1636,5 @@ static void __exit thermal_exit(void)
 	mutex_destroy(&thermal_governor_lock);
 }
 
-fs_initcall(thermal_init);
+core_initcall(thermal_init);
 module_exit(thermal_exit);
