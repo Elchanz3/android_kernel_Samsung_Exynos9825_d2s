b48b4c82a743a5734385549830f00673699d9b64
Jesse Chan <jc@lineageos.org>
2018-04-21 00:08:51 -0700

    battery: sec_battery: export {CURRENT/VOLTAGE}_MAX to sysfs

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -6411,7 +6411,9 @@ static int sec_usb_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 100000;
+
+		val->intval = battery->input_voltage * 1000000;
+
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -6498,7 +6500,9 @@ static int sec_ac_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 100000;
+
+		val->intval = battery->input_voltage * 1000000;
+
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -6554,7 +6558,9 @@ static int sec_wireless_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 100000;
+
+		val->intval = battery->input_voltage * 1000000;
+
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
