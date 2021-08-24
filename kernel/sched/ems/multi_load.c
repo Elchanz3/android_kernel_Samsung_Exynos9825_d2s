7865475e0e33731531cdb3b8ebedcfb3a689d434
MrNobody206 <simonkessler206@outlook.de>
2021-08-24 19:31:51 +0200

    sched: fix freqvar build error

--- kernel/sched/ems/multi_load.c
+++ kernel/sched/ems/multi_load.c
@@ -281,8 +281,14 @@ unsigned long ml_boosted_cpu_util(int cpu)
 
 	capacity = capacity_orig_of(cpu);
 
-	if (fv_boost > boost)
-		boost = fv_boost;
+
+#ifdef CONFIG_FREQVAR_TUNE
+	if (bg->group[STUNE_TOPAPP].tasks)
+		fv_boost = freqvar_st_boost_vector(cpu);
+#else
+	if (bg->group[STUNE_TOPAPP].tasks)
+		fv_boost = boost;
+#endif
 
 	return util + schedtune_margin(capacity, util, boost);
 }
