87c15878bdb03800e5cda892c6bbbb9dbf62afdd
Samuel Pascua <sgpascua@ngcp.ph>
2021-11-08 18:53:38 +0800

    build: compile dt.img
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- drivers/cpufreq/exynos-acme.h
+++ drivers/cpufreq/exynos-acme.h
@@ -110,7 +110,7 @@ extern int exynos_cpufreq_domain_count(void);
  * the time it takes on this CPU to switch between
  * two frequencies in nanoseconds
  */
-#define TRANSITION_LATENCY	5000000
+#define TRANSITION_LATENCY	2000000
 
 /*
  * Exynos CPUFreq API
