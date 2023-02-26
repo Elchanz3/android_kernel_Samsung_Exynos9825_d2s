9c4cc09c45defd81836c75b7e6edf713c663a55e
Phạm Tiên Sinh <song.long.dai.duong@gmail.com>
2023-02-26 17:21:01 +0800

    devfreq_boost: Apply boost when zygote forks

--- kernel/fork.c
+++ kernel/fork.c
@@ -91,6 +91,7 @@
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2135,6 +2136,7 @@ struct task_struct *fork_idle(int cpu)
 	return task;
 }
 
+extern int kp_active_mode(void);
 /*
  *  Ok, this is the main fork-routine.
  *
@@ -2151,6 +2153,9 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+    if (task_is_zygote(current))
+		devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
