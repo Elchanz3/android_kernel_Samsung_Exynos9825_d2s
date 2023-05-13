40d13b64498e82b60232dee33531b9f443fbf6a0
Elchanz3 <inutilidades639@gmail.com>
2023-05-13 15:44:54 -0300

    Revert "selinux: don't require auditing"
    This reverts commit cc63e7ff6ad1b8786ec07552a0862cd9a7100e1a.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3407,7 +3407,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3430,7 +3429,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
