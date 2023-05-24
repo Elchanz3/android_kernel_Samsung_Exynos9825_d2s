6db09be383a5e67df52b7cec9040f095d77bcb79
snapdragon teaks <inutilidades639@gmail.com>
2023-05-24 17:34:07 -0300

    clkout_exynos9820.c: increase CLKOUT offset

--- drivers/soc/samsung/cal-if/exynos9820/clkout_exynos9820.c
+++ drivers/soc/samsung/cal-if/exynos9820/clkout_exynos9820.c
@@ -1,13 +1,13 @@
 #include "clkout_exynos9820.h"
 
-#define CLKOUT0_OFFSET		(0xA00)
+#define CLKOUT0_OFFSET		(0xB04)
 #define CLKOUT0_SEL_SHIFT	(8)
 #define CLKOUT0_SEL_WIDTH	(6)
 #define CLKOUT0_SEL_TCXO	(0x1)
 #define CLKOUT0_ENABLE_SHIFT	(0)
 #define CLKOUT0_ENABLE_WIDTH	(1)
 
-#define CLKOUT1_OFFSET		(0xA0C)
+#define CLKOUT1_OFFSET		(0xAB40)
 #define CLKOUT1_SEL_SHIFT	(8)
 #define CLKOUT1_SEL_WIDTH	(6)
 #define CLKOUT1_SEL_TCXO	(0x0)
